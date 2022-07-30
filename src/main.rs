8a9db485e13e01e75531f0df8225a9d8dbbf1505
Keith Simmons <keith@the-simmons.net>
2022-07-29 22:24:24 -0700

    add hook command

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,93 @@
-#[macro_use]
-extern crate clap;
-use clap::App;
-
 use std::fs::File;
 use std::env;
 use std::path::{PathBuf};
 use std::io::{BufReader, BufRead};
+
+use clap::{Subcommand, Parser};
+use indoc::indoc;
+
 
 struct Profile {
     commands: Vec<(String, String)>,
     path: PathBuf
 }
 
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(value_parser, help = "The command in the profile to run")]
+    command: Option<String>,
+    #[clap(short, long, value_parser, help = "Prepends argument to the returned command replacing {} with the full path to the found .ok file.")]
+    prefix: Option<String>,
+    #[clap(short, long, value_parser, help = "Appends argument to the returned command replacing {} with the full path to the found .ok file.")]
+    suffix: Option<String>,
+    #[clap(short, long, value_parser, help = "Fills {n} in the matched command with the nth arguments in this list. If less than n arguments provided, empty string is substituted instead. If more than the total holes in the command are provided, then the arguments are appended to the command separated by spaces.")]
+    args: Vec<String>,
+
+    #[clap(short, long, value_parser, help = "Whether to instead of running a command, print out the hook for the given platform shell.")]
+    hook: Option<String>
+}
+
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let cli = Cli::parse();
+
+    if let Some(shell) = cli.hook {
+        let hook = match shell.as_ref() {
+            "pwsh" => Some(indoc! {r#"
+                function ok
+                {
+                    if ($args.Count -eq 0) {
+                        okeydokey | Write-Host -ForegroundColor 'Blue'
+                    } else {
+                        if ($args.Count -gt 1) {
+                            $script = okeydokey $args[0] -p "pushd {};" -s "; popd" -a ($args | select -skip 1)
+                        } else {
+                            $script = okeydokey $args[0] -p "pushd {};" -s "; popd"
+                        }
+
+                        if ($script -ne $null) {
+                            iex $script
+                        }
+                    }
+                }
+            "#}),
+            _ => None
+        };
+
+        if let Some(hook) = hook {
+            println!("{}", hook);
+        } else {
+            eprintln!("Invalid hook. Try pwsh");
+        }
+        return;
+    }
 
     let profile_opt = find_profile(env::current_dir().unwrap());
     if profile_opt.is_some() {
         let profile = profile_opt.unwrap();
 
-        match matches.value_of("COMMAND") {
-            Some(command) => query(
+        if let Some(command) = cli.command {
+            query(
                 profile,
                 command,
-                matches.value_of("prefix"),
-                matches.value_of("suffix"),
-                matches.values_of("args").map_or(Vec::new(), |args| args.collect())),
-            None => list(profile)
+                cli.prefix,
+                cli.suffix,
+                cli.args,
+            )
+        } else {
+            list(profile)
         }
+
+        // match matches.value_of("COMMAND") {
+        //     Some(command) => query(
+        //         profile,
+        //         command,
+        //         matches.value_of("prefix"),
+        //         matches.value_of("suffix"),
+        //         matches.values_of("args").map_or(Vec::new(), |args| args.collect())),
+        //     None => list(profile)
+        // }
     }
 }
 
@@ -75,10 +134,10 @@ fn list(profile: Profile) {
     println!("{}", list.trim());
 }
 
-fn query(profile: Profile, command: &str, prefix: Option<&str>, suffix: Option<&str>, args: Vec<&str>) {
+fn query(profile: Profile, command: String, prefix: Option<String>, suffix: Option<String>, args: Vec<String>) {
     let best_option = profile.commands
         .iter()
-        .filter_map(|(possible_command, _)| shared_prefix(possible_command, command))
+        .filter_map(|(possible_command, _)| shared_prefix(possible_command, &command))
         .max_by_key(|&(shared_chars, _)| shared_chars);
 
     match best_option {
@@ -94,7 +153,7 @@ fn shared_prefix(possible_command: &str, command: &str) -> Option<(usize, String
     }
 }
 
-fn print_decorated_command(profile: Profile, command_name: String, prefix: Option<&str>, suffix: Option<&str>, args: Vec<&str>) {
+fn print_decorated_command(profile: Profile, command_name: String, prefix: Option<String>, suffix: Option<String>, args: Vec<String>) {
     let prefix = fill_in_profile_directory(&profile, prefix);
     let suffix = fill_in_profile_directory(&profile, suffix);
     let (_, command) = profile.commands
@@ -105,7 +164,7 @@ fn print_decorated_command(profile: Profile, command_name: String, prefix: Optio
     println!("{}", vec![prefix, fill_in_arguments(command.to_string(), args), suffix].concat())
 }
 
-fn fill_in_profile_directory(profile: &Profile, pattern: Option<&str>) -> String {
+fn fill_in_profile_directory(profile: &Profile, pattern: Option<String>) -> String {
     let profile_directory = profile.path.parent().unwrap().to_str().unwrap();
     pattern.unwrap_or_default().replace("{}", profile_directory)
 }
@@ -125,7 +184,7 @@ fn count_holes(command: &String) -> usize {
     rec(command, 0)
 }
 
-fn fill_in_arguments(perferated_command: String, args: Vec<&str>) -> String {
+fn fill_in_arguments(perferated_command: String, args: Vec<String>) -> String {
     let number_of_holes = count_holes(&perferated_command);
     let mut args_iterator = args.iter();
     let mut command = perferated_command;
